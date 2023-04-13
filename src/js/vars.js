d23b9df45581229bce5507fc5f10fded97486d5a
Житінський Владислав <114184074+vlad2000j@users.noreply.github.com>
2023-04-13 16:51:23 +0300

    vars

--- src/js/vars.js
+++ src/js/vars.js
@@ -1,4 +1,8 @@
+import Notiflix from 'notiflix';
+import axios from 'axios';
+import SimpleLightbox from "simplelightbox";
+
 // Ключ API 
-const API_KEY ='2ac41627e60ee28ce7ee19eda978da51';
+const API_KEY = '2ac41627e60ee28ce7ee19eda978da51';
 // Приклад API-запиту
 'https://api.themoviedb.org/3/movie/550?api_key=2ac41627e60ee28ce7ee19eda978da51';
