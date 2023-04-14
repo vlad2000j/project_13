f3dacac84931f055f39f7703092a6b7d63981998
angelkapv81@gmail.com <angelkapv81>
2023-04-14 08:54:20 +0300

    add example of usege

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 import './js/vars';
 import ApiService from './js/api-service';
 
-// myService = new ApiService();
-// myService.query = 'Dos';
-// myService.incrementPage();
-// async function markup() {
-//   const movieData = await myService.getMoviesData();
-//   console.log(movieData.page);
-//   movieData.movies.forEach(element => {
-//     console.log('--------------------------');
-//     console.log(element.title);
-//     console.log(element.genres_names);
-//     console.log(element.poster_url);
-//     console.log(element.release_year);
-//   });
-// }
+myService = new ApiService();
+myService.query = 'Dos';
 
-// markup();
+async function cardsMarkup() {
+  const movieData = await myService.getMoviesData();
+  console.log(movieData.page);
+  const movieCards = movieData.movies
+    .map(
+      element =>
+        `<div>
+        <img src=${element.poster_url}/>
+        <p>${element.title}
+        <p><span>${element.genres_names.join(', ')}</span> | <span>${
+          element.release_year
+        }</span></p>
+      </div>`
+    )
+    .join('');
+  console.log(movieCards); // Замість console.log можна insertAdjusentHTML
+}
+
+cardsMarkup();
---
+++ src/js/api-service.js
@@ -0,0 +1,64 @@
+import axios from 'axios';
+import API_KEY from './vars';
+import genres from './genres';
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+export default class ApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  async getMoviesData() {
+    const config = {
+      method: 'get',
+      url: `${BASE_URL}/search/movie`,
+      responseType: 'json',
+      params: {
+        api_key: API_KEY,
+        query: this.searchQuery,
+        page: this.page,
+      },
+    };
+
+    const response = await axios(config);
+    this.incrementPage();
+    const movies = [];
+    for (const obj of response.data.results) {
+      const { genre_ids, title, poster_path, release_date } = obj;
+      const poster_url = `https://image.tmdb.org/t/p/original/${poster_path}`;
+      const genres_names = getGenresByIds(genres, genre_ids);
+      const release_year = release_date.slice(0, 4);
+      const new_obj = { genres_names, title, poster_url, release_year };
+      movies.push(new_obj);
+    }
+    const data = {
+      page: response.data.page,
+      movies,
+    };
+    return data;
+  }
+}
+// ----------------------------------------------------------------------------
+
+function getGenresByIds(genres, ids) {
+  return genres
+    .filter(genre => ids.includes(genre.id))
+    .map(genre => genre.name);
+}
