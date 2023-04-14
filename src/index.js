2972a13419d3e72e5322271d0642114c6d09fe6b
angelkapv81@gmail.com <angelkapv81>
2023-04-14 07:11:12 +0300

    added API service

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './js/vars';
+import ApiService from './js/api-service';
+
+// myService = new ApiService();
+// myService.query = 'Dos';
+// myService.incrementPage();
+// async function markup() {
+//   const movieData = await myService.getMoviesData();
+//   console.log(movieData.page);
+//   movieData.movies.forEach(element => {
+//     console.log('--------------------------');
+//     console.log(element.title);
+//     console.log(element.genres_names);
+//     console.log(element.poster_url);
+//     console.log(element.release_year);
+//   });
+// }
+
+// markup();
--- src/js/vars.js
+++ src/js/vars.js
@@ -1,8 +1,10 @@
 import Notiflix from 'notiflix';
 import axios from 'axios';
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
 
-// Ключ API 
+// Ключ API
 const API_KEY = '2ac41627e60ee28ce7ee19eda978da51';
 // Приклад API-запиту
-'https://api.themoviedb.org/3/movie/550?api_key=2ac41627e60ee28ce7ee19eda978da51';
+('https://api.themoviedb.org/3/movie/550?api_key=2ac41627e60ee28ce7ee19eda978da51');
+
+export default API_KEY;
