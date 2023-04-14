3c0b2c6ab884089815470c17fc6973d39acfb6a3
angelkapv81@gmail.com <angelkapv81>
2023-04-14 10:40:37 +0300

    add more comments

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -94,6 +94,41 @@ export default class ApiService {
 // original_title --- оришінальна назва (для модалки)
 // vote_average --- середній рейтинг (для модалки)
 // vote_count --- кількість голосів (для модалки)
+// ----------------------------------------------------------------------------
+
+// Приклад використання
+
+// myService = new ApiService();
+// myService.query = 'Titanic'; // Пошуковий запит
+// myService.page = 2; // Номер сторінки
+
+// Для картотеки фільмів
+
+// async function cardsMarkup() {
+//   const movieData = await myService.getMoviesData();
+//   if (!movieData.movies.length) {
+//     console.log('No such movies'); // Тут можна Notify, або якусь модалку
+//     return;
+//   }
+//   console.log(movieData.page);
+//   const movieCards = movieData.movies
+//     .map(
+//       element =>
+//         `<div>
+//         <img src=${element.poster_url}/>
+//         <p>${element.title}</p>
+//         <p><span>${element.genres_names.join(', ')}</span> | <span>${
+//           element.release_year
+//         }</span></p>
+//       </div>`
+//     )
+//     .join('');
+//   console.log(movieCards); // Замість console.log можна insertAdjusentHTML
+// }
+
+// cardsMarkup();
+
+// ----------------------------------------------------------------------------
 
 // Функція для перетворення масиву ids жанрів в масив імен жанрів
 function getGenresByIds(genres, ids) {
