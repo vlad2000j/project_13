d83ded89c9e050686ce942107dc8b747b339fbe2
angelkapv81@gmail.com <angelkapv81>
2023-04-15 10:35:32 +0300

    add removeQueue, fix bugs

---
+++ src/js/queue-service.js
@@ -0,0 +1,59 @@
+//приклад використання
+
+// Імпортужмо в index.js
+
+// import QueueService from './js/queue-service';
+
+// Створюємо об'єкт
+
+// myQueue = new QueueService();
+
+// myQueue.removeQueue(); // Очищуємо чергу
+// myQueue.setQueue({ id: 123, title: 'More' }); // Додаємо дані фільму в чергу
+// myQueue.setQueue({ id: 124, title: 'More' }); // Додаємо дані фільму в чергу
+// myQueue.removeFromQueue(123); // Видаляємо фільм з черги за id
+// myQueue.removeFromQueue(124); // Видаляємо фільм з черги за id
+// myQueue.setQueue({ id: 234, title: 'Іndiana Johnes' }); // Додаємо дані фільму в чергу
+
+QUEUE = 'queue';
+export default class QueueService {
+  setQueue(movie) {
+    if (localStorage.getItem(QUEUE)) {
+      // Читаємо з localStorage d movie
+      const movies = JSON.parse(localStorage.getItem(QUEUE));
+      //Перевіряємо, чи є вже такий об'єкт в черзі
+      if (!movies.some(obj => obj.id === movie.id)) {
+        // Додаємо об'єкт до масиву movies
+        movies.push(movie);
+        // Додаємо оновлений масив до localStorage
+        localStorage.setItem(QUEUE, JSON.stringify(movies));
+      }
+    } else {
+      localStorage.setItem(QUEUE, JSON.stringify([movie]));
+    }
+  }
+
+  getAllQueue() {
+    if (localStorage.getItem(QUEUE)) {
+      return JSON.parse(localStorage.getItem(QUEUE));
+    }
+  }
+
+  getQueueById(id) {
+    if (localStorage.getItem(QUEUE)) {
+      // const movies = JSON.parse(localStorage.getItem(QUEUE));
+      return this.getAllQueue().filter(e => e.id === id);
+    }
+  }
+
+  removeFromQueue(id) {
+    if (localStorage.getItem(QUEUE)) {
+      const newMovies = this.getAllQueue().filter(obj => obj.id !== id);
+      localStorage.setItem(QUEUE, JSON.stringify(newMovies));
+    }
+  }
+
+  removeQueue() {
+    localStorage.removeItem(QUEUE);
+  }
+}
