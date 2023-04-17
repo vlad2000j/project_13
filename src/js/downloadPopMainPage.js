1ad7345516fa01b99e3f252e70711f76b164b948
Юрій-Юркевич <yurko0510yurko@gmail.com>
2023-04-17 17:29:30 +0300

    Update downloadPopMainPage.js

--- src/js/downloadPopMainPage.js
+++ src/js/downloadPopMainPage.js
@@ -1,21 +1,12 @@
-<<<<<<< Updated upstream
-=======
+import axios from 'axios';
+import { API_KEY } from './vars';
 
-import axios from "axios";
-// import { pathParams } from "./vars";
-
-const pathParams = {
-  API_KEY:  '2ac41627e60ee28ce7ee19eda978da51',
-  BASE_URL: 'https://api.themoviedb.org/3/',
-  media_type: 'movie',
-  time_window: 'week',
-
-}
+export { downloadPopMainPage };
+  
+const BASE_URL = 'https://api.themoviedb.org/3/';
 
 const getPopularMovies = async page => {
   return await axios.get(
     `${BASE_URL}trending/movie/day?api_key=${API_KEY}&page=${page}`
   );
 };
-// import './js/downloadPopMainPage';
->>>>>>> Stashed changes
